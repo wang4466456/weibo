e66a3c45f788dda16e3c44e172d7be76c17ae0b6
Ms wang <wang4466595@icloud.com>
2023-10-20 08:36:20 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+//用户注册
+Route::get('signup', 'UsersController@create')->name('signup');
+
