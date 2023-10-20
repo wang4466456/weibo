05d1a51a3882d473ef6dda09ec175bb70e4db48f
Ms wang <wang4466595@icloud.com>
2023-10-20 16:20:58 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 //用户注册
 Route::get('signup', 'UsersController@create')->name('signup');
 
+Route::resource('users', 'UsersController');
+
+
